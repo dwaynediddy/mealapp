2bc75bdc59e11dd6e21dc0143f3714486a8160ca
dwaynediddy <burrows.dwayne@icloud.com>
2020-10-28 22:41:00 +1000

    added more sudo

--- src/screens/Plan.js
+++ src/screens/Plan.js
@@ -4,11 +4,16 @@ import Breakfast from './components/Breakfast'
 
 //conditional rendering from form results
 const generateMeals = () => {
-// if btn-1 && || btn-3 === true
+
+    //switch statement??
+    // if btn-1 && || btn-3 === true
     //return <Breakfast /> and lunch and dinner and post workout
     // if combination of buttons === true return lunch and dinner
     //most combintaions to
     //else show lunch and dinner
+    //need to first of all know what to render via form
+    //on breakfast page
+    //then IF that meals is rendered at all on this page
 }
 
 const Plan = () => {
